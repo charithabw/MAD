9aab82aad1378a8abb3108db4d724d9296a36f12
sashii1998 <53555047+sashii1998@users.noreply.github.com>
2019-08-06 01:29:37 +0530

    add next btn

--- app/src/main/java/com/example/mad/Level_01.java
+++ app/src/main/java/com/example/mad/Level_01.java
@@ -1,7 +1,9 @@
 package com.example.mad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Level_01 extends AppCompatActivity {
@@ -13,5 +15,27 @@ public class Level_01 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_01);
         nextBtn=(Button)findViewById(R.id.btnNext);
+        prvBtn=(Button)findViewById(R.id.btnBack);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(getApplicationContext(),Level_02.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+        prvBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
