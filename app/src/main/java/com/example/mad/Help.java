0b2e88fd8ae5f87f0a233aca3b3a50a136755e60
charithabw <52483097+charithabw@users.noreply.github.com>
2019-08-06 09:52:52 +0530

    Add help, about, backgrounds and toast

---
+++ app/src/main/java/com/example/mad/Help.java
@@ -0,0 +1,27 @@
+package com.example.mad;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Help extends AppCompatActivity {
+    Button prvBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        prvBtn = (Button)findViewById(R.id.btnBack) ;
+        prvBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/mad/MainActivity.java
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -6,14 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button nextbutton;
+    Button nextbutton, aboutbutton, helpbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nextbutton = (Button)findViewById(R.id.button);
+        aboutbutton = (Button)findViewById(R.id.btnAbout);
+        helpbutton = (Button)findViewById(R.id.btnHelp);
 
 
        nextbutton.setOnClickListener(new View.OnClickListener() {
@@ -23,12 +26,35 @@ public class MainActivity extends AppCompatActivity {
                Intent intent =new Intent(getApplicationContext(),Level_01.class);
                startActivity(intent);
                finish();
+               Toast.makeText(MainActivity.this, "Start the GAME", Toast.LENGTH_SHORT).show();
 
            }
        });
-    }
+        aboutbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(getApplicationContext(),About.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        helpbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(getApplicationContext(),Help.class);
+                startActivity(intent);
+
+                finish();
+                Toast.makeText(MainActivity.this, "help", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
 
-}
+
+}}
