3c29660bce47904f152f7e37ae7652808fa6a7f6
dinithi1997 <dinithianupama1@gmail.com>
2019-09-23 18:36:42 +0530

    DB connect

--- app/src/main/java/com/example/mad/Level_03.java
+++ app/src/main/java/com/example/mad/Level_03.java
@@ -3,10 +3,13 @@ package com.example.mad;
 import android.content.ClipData;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -35,12 +38,17 @@ public class Level_03 extends AppCompatActivity implements SensorEventListener {
     Button btnOk, nextBtn, prvBtn;
     FrameLayout r2;
     ImageView iv;
-    TextView score;
+    TextView score,timer;
     FrameLayout.LayoutParams params2;
 
     ImageView upperImageViews;
     ImageView lowerImageView[] = new ImageView[5];
     private static int iScore = 10;
+    private static String iLevel = "3_1";
+
+    CountDownTimer countDownTimer;
+    long timeLeftInMilliSeconds = 15000;
+    boolean timeRunning;
 
 
     @Override
@@ -57,9 +65,9 @@ public class Level_03 extends AppCompatActivity implements SensorEventListener {
         score = (TextView) findViewById(R.id.txtScore);
         score.setText(String.valueOf(iScore));
         btnOk = (Button) findViewById(R.id.btnOk);
-        nextBtn=(Button)findViewById(R.id.btnNext);
-        prvBtn=(Button)findViewById(R.id.btnBack);
-
+        nextBtn = (Button) findViewById(R.id.btnNext);
+        prvBtn = (Button) findViewById(R.id.btnBack);
+        timer=(TextView) findViewById(R.id.txtTimer) ;
         upperImageViews = (ImageView) findViewById(R.id.imgBox);
 
         lowerImageView[0] = (ImageView) findViewById(R.id.imgNum_20);
@@ -67,6 +75,8 @@ public class Level_03 extends AppCompatActivity implements SensorEventListener {
         lowerImageView[2] = (ImageView) findViewById(R.id.imgNum_40);
         lowerImageView[3] = (ImageView) findViewById(R.id.imgNum_50);
         lowerImageView[4] = (ImageView) findViewById(R.id.imgNum_60);
+
+        startTime();
 
         int upperImages[][] = {
                 {R.drawable.box}
@@ -114,16 +124,36 @@ public class Level_03 extends AppCompatActivity implements SensorEventListener {
                     public void onClick(DialogInterface dialogInterface, int i) {
                         //finish();
                         addData();
+                        getAllInfo();
+                        stopTimer();
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                //Do something after 100ms
+                                Intent intent = new Intent(getApplicationContext(), Level_03_2.class);
+                                intent.putExtra("Score", getiScore());
+                                startActivityForResult(intent, 1);
+                                finish();
+                                iScore = 10;
+                            }
+                        }, 5000);
+
+                                iScore = 10;
                     }
+
+
+
                 })
                         .setNegativeButton("No", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 dialogInterface.cancel();
+
                             }
                         });
                 AlertDialog alertDialog = builder.create();
-                alertDialog.setTitle("Your Score " +iScore);
+                alertDialog.setTitle("Your Score " + iScore);
                 alertDialog.show();
             }
         });
@@ -131,7 +161,7 @@ public class Level_03 extends AppCompatActivity implements SensorEventListener {
             @Override
             public void onClick(View view) {
 
-                Intent intent =new Intent(getApplicationContext(),Level_03_2.class);
+                Intent intent = new Intent(getApplicationContext(), Level_03_2.class);
                 startActivity(intent);
                 finish();
 
@@ -141,12 +171,86 @@ public class Level_03 extends AppCompatActivity implements SensorEventListener {
             @Override
             public void onClick(View view) {
 
-                Intent intent =new Intent(getApplicationContext(),Level_03.class);
+                Intent intent = new Intent(getApplicationContext(), Level_03.class);
                 startActivity(intent);
                 finish();
 
             }
         });
+    }
+        public void startTime() {
+            if (timeRunning) {
+                stopTimer();
+                // Toast.makeText(Level_04.this, "ansfknak", Toast.LENGTH_SHORT).show();
+
+
+            } else
+                startTimer();
+            // Toast.makeText(Level_04.this, "kkkkkk", Toast.LENGTH_SHORT).show();
+        }
+
+        public void startTimer() {
+            countDownTimer = new CountDownTimer(timeLeftInMilliSeconds, 1000) {
+                @Override
+                public void onTick(long l) {
+                    timeLeftInMilliSeconds = l;
+                    updateTimes();
+                }
+
+                @Override
+                public void onFinish() {
+                    Toast.makeText(Level_03.this, "Times gone", Toast.LENGTH_SHORT).show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Level_03.this);
+                    builder.setMessage("Do you want to Submit !!!").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            addData();
+                            //finish();
+                        }
+
+                    })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Intent intent = new Intent(getApplicationContext(), Level_03_2.class);
+                                    startActivity(intent);
+                                    dialogInterface.cancel();
+                                    //finish();
+
+                                }
+                            });
+                    AlertDialog alertDialog = builder.create();
+
+                    alertDialog.setTitle("Your Score " + iScore);
+                    alertDialog.show();
+                }
+            }.start();
+            timeRunning = true;
+        }
+
+        public void stopTimer() {
+            countDownTimer.cancel();
+            timeRunning = false;
+        }
+
+        public void updateTimes() {
+            final Handler handler = new Handler();
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    int minutes = (int) timeLeftInMilliSeconds / 60000;
+                    int seconds = (int) timeLeftInMilliSeconds % 60000 / 1000;
+                    String timeLeftText;
+
+                    timeLeftText = "" + minutes;
+                    timeLeftText += ":";
+                    if (seconds < 10) timeLeftText += "0";
+                    timeLeftText += seconds;
+
+                    timer.setText(timeLeftText);
+                }
+            });
 
     }
 
@@ -282,8 +386,8 @@ public class Level_03 extends AppCompatActivity implements SensorEventListener {
 
                     } else if (view.getId() == R.id.imgNum_30) {
                         Toast.makeText(Level_03.this, "30", Toast.LENGTH_SHORT).show();
-                        upperImageViews.setImageResource(R.drawable.box);
-                        iScore = iScore + 2;
+                        upperImageViews.setImageResource(R.drawable.redbox);
+                        iScore = iScore - 2;
                     } else if (view.getId() == R.id.imgNum_40) {
                         Toast.makeText(Level_03.this, "40", Toast.LENGTH_SHORT).show();
                         upperImageViews.setImageResource(R.drawable.box);
@@ -317,10 +421,83 @@ public class Level_03 extends AppCompatActivity implements SensorEventListener {
     public int getiScore(){
         return iScore;
     }
-    public void addData(){
-       // myDB.addInfo("charithamm", "bandara", "1", "14");
-        Toast.makeText(Level_03.this, "Success" ,Toast.LENGTH_SHORT).show();
+    public void addData() {
+        String score = Integer.toString(iScore);
+        //String name2 = name;
+
+        boolean data = myDB.addInfo_Score_Table("", "",iLevel,score);
+        if (data = true) {
+            Toast.makeText(Level_03.this, "Success", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(Level_03.this, "Invalid", Toast.LENGTH_SHORT).show();
+        }
     }
+
+
+    public void getAllInfo() {
+
+
+
+        try {
+            // Toast.makeText(Level_04.this, "Data Not Found", Toast.LENGTH_SHORT).show();
+            Cursor cursor = myDB.getInfo_Score_Table();
+            Cursor cursor1 = myDB.getHieghestSocre();
+
+            if (cursor.getCount() == 0) {
+                Toast.makeText(Level_03.this, "Data Not Found", Toast.LENGTH_SHORT).show();
+                showMassages("Error", "Data not Found!!!");
+                return;
+            } else {
+
+
+                Toast.makeText(Level_03.this, "Data Found", Toast.LENGTH_SHORT).show();
+
+                StringBuffer buffer = new StringBuffer();
+                StringBuffer buffer1 = new StringBuffer();
+
+                while (cursor.moveToNext()) {
+                    buffer.append("Score: " + cursor.getString(4) + "\n");
+                    // buffer.append(""+cursor.getString(1)+")\n");
+
+                    //cursor.close();
+
+                }
+                cursor.close();
+                while (cursor1.moveToNext()) {
+                    buffer1.append("Highest Sore: " + cursor1.getString(4));
+                    // buffer1.append(""+cursor1.getString(1)+")");
+                }
+                cursor1.close();
+                showMassages("" + buffer1.toString(), buffer.toString());
+
+            }
+
+
+        } catch(Exception e)
+
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(true);
+            builder.setTitle("Error");
+            builder.setMessage("" + e);
+            builder.show();
+        }
+    }
+
+
+
+    public void showMassages(String title, String message) {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+
 
 
 
