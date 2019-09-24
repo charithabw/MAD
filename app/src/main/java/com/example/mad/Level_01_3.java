d527ea5cc250f9297fbbaf0f4159a2ce3bafc442
isurugunarathna <50676533+isurugunarathna@users.noreply.github.com>
2019-09-24 13:24:09 +0530

    level 1 Btn update

--- app/src/main/java/com/example/mad/Level_01_3.java
+++ app/src/main/java/com/example/mad/Level_01_3.java
@@ -8,6 +8,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
@@ -32,7 +33,7 @@ public class Level_01_3 extends AppCompatActivity implements SensorEventListener
     private long lastUpdate;
 
 
-    // MediaPlayer player;
+     MediaPlayer player;
 
     private int gameValue = 1;
     Button btnOk, nextBtn, prvBtn;
@@ -124,6 +125,7 @@ public class Level_01_3 extends AppCompatActivity implements SensorEventListener
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                play(view);
                 AlertDialog.Builder builder = new AlertDialog.Builder(Level_01_3.this);
                 builder.setMessage("Do you want to Submit !!!").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
@@ -133,6 +135,7 @@ public class Level_01_3 extends AppCompatActivity implements SensorEventListener
                         getAllInfo();
                         stopTimer();
 
+                        stopPlayer();
                         final Handler handler = new Handler();
                         handler.postDelayed(new Runnable() {
                             @Override
@@ -536,7 +539,35 @@ public class Level_01_3 extends AppCompatActivity implements SensorEventListener
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+    public void play(View view){
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.click_sound);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    stopPlayer();
+                }
+            });
+        }
+        player.start();
+    }
+    public void stop(View view){
+        stopPlayer();
+    }
+    private void stopPlayer(){
+        if (player != null){
+            player.release();
+            player = null;
+            Toast.makeText(this, "Sound stop",Toast.LENGTH_SHORT).show();
+        }
+    }
 
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
+    }
 }
+
+
 
